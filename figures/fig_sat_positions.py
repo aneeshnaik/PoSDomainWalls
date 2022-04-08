d5e7a6d11c4029ab7760d6606249765dc037cded
Aneesh Naik <aneesh92@hotmail.co.uk>
2022-04-08 11:32:31 +0100

    Add indiv lines to fig 1

--- figures/fig_sat_positions.py
+++ figures/fig_sat_positions.py
@@ -31,12 +31,14 @@ if __name__ == '__main__':
     plt.rcParams['font.size'] = 9
     plt.rcParams['ytick.labelsize'] = 8
     plt.rcParams['xtick.labelsize'] = 8
-    c0 = 'goldenrod'
+    c0 = 'rebeccapurple'
     c1 = 'teal'
-    sargs0 = {'s': 1, 'c': c0, 'rasterized': True}
+    sargs0 = {'s': 1, 'c': c1, 'rasterized': True}
     sargs1 = {'s': 1, 'c': c1, 'rasterized': True}
-    largs0 = {'c': c0, 'alpha': 0.05, 'rasterized': True}
+    largs0 = {'c': c1, 'alpha': 0.05, 'rasterized': True}
     largs1 = {'c': c1, 'alpha': 0.05, 'rasterized': True}
+    arrargs = {'fc': 'k', 'width': 2, 'zorder': 10,
+               'length_includes_head': True}
 
     # set up figure
     asp = 7 / 6.75
@@ -60,6 +62,12 @@ if __name__ == '__main__':
     ax01.plot(x0[:, :500, 0], x0[:, :500, 2], **largs0)
     ax11.plot(x1[:, :500, 0], x1[:, :500, 2], **largs1)
 
+    # highlights
+    ax01.plot(x0[:, 0, 0], x0[:, 0, 2], c=c0, lw=0.8, alpha=0.9, zorder=8)
+    ax01.plot(x0[:, 9, 0], x0[:, 9, 2], c='k', lw=0.6, alpha=0.5, zorder=5)
+    ax11.plot(x1[:, 0, 0], x1[:, 0, 2], c=c0, lw=0.8, alpha=0.9, zorder=8)
+    ax11.plot(x1[:, 9, 0], x1[:, 9, 2], c='k', lw=0.6, alpha=0.5, zorder=5)
+
     # labels, limits etc.
     for ax in fig.axes:
         ax.set_xlim(-420, 420)
@@ -75,6 +83,11 @@ if __name__ == '__main__':
     ax11.set_xlabel(r"$x\ [\mathrm{kpc}]$")
     ax00.set_title("No domain wall", fontsize=11)
     ax10.set_title("Domain wall at $z=0$", fontsize=11)
+    t = ax11.transAxes
+    ax11.text(0.22, 0.18, r"\textbf{`On-plane' orbit}", transform=t)
+    ax11.text(0.5, 0.75, r"\textbf{`Off-plane' orbit}", transform=t)
+    ax11.arrow(-100, -220, 12, 160, **arrargs)
+    ax11.arrow(60, 200, -10, -25, **arrargs)
 
     # save figure
     fig.savefig("fig_sat_positions.pdf")
